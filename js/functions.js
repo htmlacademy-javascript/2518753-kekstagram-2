bb4d79b11f64f1800b5fe1a7409eb2e6064e60d5
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-17 18:48:23 +0300

    исправляет 2ую задачу

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+// Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и
+//  максимальную длину и возвращает true, если строка меньше или равна указанной длине,
+//  и false, если строка длиннее. Эта функция нам пригодится для валидации формы.
+//  Примеры использования функции:
+
+const checkingStringLength = (line = '', maximumLength = 1) => line.length <= maximumLength;
+// Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
+
+const isPalindrome = (line) => {
+  line = line.replaceAll(' ', '').toLowerCase();
+  let reversedLine = '';
+  for (let i = line.length - 1; i >= 0; i--) {
+    reversedLine += line[i];
+  }
+  return line === reversedLine;
+};
