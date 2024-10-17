7af4a335f1da4693b974d44babec7fb811ba625d
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-17 18:48:23 +0300

    исправляет 2ую задачу

--- js/functions.js
+++ js/functions.js
@@ -7,10 +7,10 @@ const checkingStringLength = (line = '', maximumLength = 1) => line.length <= ma
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
 
 const isPalindrome = (line) => {
-  line = line.replaceAll(' ','').toLowerCase();
+  line = line.replaceAll(' ', '').toLowerCase();
   let reversedLine = '';
   for (let i = line.length - 1; i >= 0; i--) {
     reversedLine += line[i];
-    return line === reversedLine;
   }
+  return line === reversedLine;
 };
