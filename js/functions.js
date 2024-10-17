fc091f1a5854877cd5d5e1a8dd972f6d399f6296
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-17 18:34:17 +0300

    решает задачу 2

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,14 @@
 //  и false, если строка длиннее. Эта функция нам пригодится для валидации формы.
 //  Примеры использования функции:
 
-function stringLength(line,maxLine) {
-  return line.length <= maxLine;
-}
+const checkingStringLength = (line = '', maximumLength = 1) => line.length <= maximumLength;
+// Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
 
+const isPalindrome = (line) => {
+  line = line.replaceAll(' ','').toLowerCase();
+  let reversedLine = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedLine += string[i];
+    return string === reversedLine;
+  }
+};
