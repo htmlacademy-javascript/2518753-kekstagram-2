bd9963e9e1186516d5399cdab792c0d1cc10bfbf
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-18 12:27:58 +0300

    выполняет задание 3

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@
 const checkingStringLength = (line = '', maximumLength = 1) => line.length <= maximumLength;
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
 
-const isPalindrome = (line) => {
+const isPalindrome = (line = '') => {
   line = line.replaceAll(' ', '').toLowerCase();
   let reversedLine = '';
   for (let i = line.length - 1; i >= 0; i--) {
@@ -14,3 +14,13 @@ const isPalindrome = (line) => {
   }
   return line === reversedLine;
 };
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
+
+function extractDigitsToNumber (inputString) {
+  const digits = inputString.math(/\d+/g);
+  if (digits) {
+    return parseInt(digits.join(''),10);
+  } else {
+    return NaN;
+  }
+}
