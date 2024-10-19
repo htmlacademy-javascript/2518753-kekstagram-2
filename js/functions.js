70edf2fe52b4b87e3ea32551ea7248dfb4bf7d85
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-19 11:40:43 +0300

    вносит изменения

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,10 @@
 //  и false, если строка длиннее. Эта функция нам пригодится для валидации формы.
 //  Примеры использования функции:
 
-const checkingStringLength = (line = '', maximumLength = 1) => line.length <= maximumLength;
+const checkingStringLength = (line, maximumLength = 1) => line.length <= maximumLength;
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево
 
-const isPalindrome = (line = '') => {
+const isPalindrome = (line) => {
   line = line.replaceAll(' ', '').toLowerCase();
   let reversedLine = '';
   for (let i = line.length - 1; i >= 0; i--) {
@@ -14,10 +14,16 @@ const isPalindrome = (line = '') => {
   }
   return line === reversedLine;
 };
+
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const reversedString = normalizedString.split('').reverse().join('');
+  return normalizedString === reversedString;
+};
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
 
-function extractDigitsToNumber (inputString) {
-  const digits = inputString.math(/\d+/g);
+const extractDigitsToNumber = (inputString) => {
+  const digits = inputString.match(/\d+/g);
   if (digits) {
     return parseInt(digits.join(''),10);
   } else {
