59ed3b5444fcc517e46c7fc2656dcd547793a30e
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-11-28 13:52:47 +0300

    лайк для одной фото

--- js/showBigPicture.js
+++ js/showBigPicture.js
@@ -1,22 +1,21 @@
 import {picturesContainer} from'./createPhotos';
 import './createPhotos.js';
-
+import './data.js';
 
 const bigWindow = document.querySelector('.big-picture');
 const smallPictures = picturesContainer.querySelectorAll('.picture__img');
 const pictureCloseButton = document.querySelector('#picture-cancel');
 const bigPicture = bigWindow.querySelector('.big-picture__img img');
-const smallPicture = picturesContainer.querySelector('.picture__img');
+const bigPictureSocial = document.querySelector('.social__header');
+const socialComment = document.querySelector('.social__comments');
+const likesCounts = document.querySelector('.likes-count');
+const likesElements = document.querySelector('.picture__likes');
 
-// function changeImage(newSrc) {
-//   bigPictureImg.src = newSrc;
-// }
 
 function openBigPicture (element){
   bigWindow.classList.remove('hidden');
   bigPicture.src = element.src ;
 }
-
 
 function closeBigPicture(){
   bigWindow.classList.add('hidden');
@@ -28,10 +27,18 @@ function closeBigPictureEsc(evt){
   }
 }
 
+function likesCountFunction(likesCount,likesElement){
+  likesCount.textContent = likesElement.textContent;
+}
+
+
 smallPictures.forEach((smallPicture) => {
 
   smallPicture.addEventListener('click', () => openBigPicture(smallPicture));
+  likesCounts.textContent = likesElements.textContent;
 });
+
+
 pictureCloseButton.addEventListener('click',closeBigPicture);
 document.addEventListener('keydown', closeBigPictureEsc);
 
