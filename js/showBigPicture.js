ba598ed9b44e2ca5da70da7f2fd0eb0cba5b03fa
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-12-01 13:49:04 +0300

    загружает по 5 комментариев. пока одинаковые

--- js/showBigPicture.js
+++ js/showBigPicture.js
@@ -5,9 +5,12 @@ const btnWindowCancel = bigWindow.querySelector('.big-picture__cancel');
 const bigWindowTotalComment = document.querySelector('.social__comment-total-count');
 const bigWindowCountComment = document.querySelector('.social__comment-count');
 const bigWindowComments = bigWindow.querySelector('.social__comments');
-
+const socialCaption = bigWindow.querySelector('.social__caption');
+const commentsLoader = bigWindow.querySelector('.comments-loader');
+const btnCommentsLoader = bigWindow.querySelector('.comments-loader');
 function closeBigPicture(){
   bigWindow.classList.add('hidden');
+
 }
 
 function closeBigPictureEsc(evt){
@@ -16,27 +19,39 @@ function closeBigPictureEsc(evt){
   }
 }
 
+
+function currentListComments (comments){
+
+  const currentIndex = 0;
+  const endIndex = 5;
+  const currentList = comments.slice(currentIndex, currentIndex + endIndex);
+  btnCommentsLoader.addEventListener('click',() =>{
+    currentListComments(comments);
+  });
+
+  currentList.forEach(({ id, avatar, name, message }) => {
+    bigWindowComments.insertAdjacentHTML('beforeend', `
+        <li class="social__comment" id="comment-${id}">
+            <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
+            <p class="social__text">${message}</p>
+        </li>
+    `);
+  });
+}
+
+
 export const showBigPicture = ({url, description, likes, comments})=>{
   bigWindow.classList.remove('hidden');
-  bigWindowCountComment.classList.add('hidden');
+  commentsLoader.classList.remove('hidden');
+
   bigWindowComments.innerHTML = '';
   bigWindowImg.src = url;
   bigWindowLikes.textContent = likes;
   bigWindowTotalComment.textContent = comments.length;
-  console.log(comments);
+  socialCaption.textContent = description;
+  document.body.className = 'modal-open';
+  currentListComments (comments);
 
-  comments.forEach(({id, avatar, name, message}) => {
-    bigWindowComments.insertAdjacentHTML('afterbegin',`
-        <li class="social__comment" id="comment-${id}">
-          <img
-            class="social__picture"
-            src="${avatar}"
-            alt="${name}"
-            width="35" height="35">
-          <p class="social__text">${message}</p>
-        </li>
-      `);
-  });
   btnWindowCancel.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', closeBigPictureEsc);
 
