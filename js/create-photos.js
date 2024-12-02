e47bf86a8d19c2e331cf5dde4cc633bce09a3b28
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-12-02 14:09:28 +0300

    Module8-task1 (#12)
    * заменяем.открываем.закрываем
    
    * добавил на каждую фото
    
    * лайк для одной фото
    
    * меняет значение?
    
    * изменяет модуль.
    
    * загружает по 5 комментариев. пока одинаковые
    
    * исправляет
    
    * делает счетчик
    
    * вносит исправления
    
    * убирает лишнее
    
    * добавляет исправления
    
    * убирает пробел

--- js/create-photos.js
+++ js/create-photos.js
@@ -1,4 +1,6 @@
 import { userPhotos } from './data.js';
+import { showBigPicture } from './show-big-picture.js';
+
 
 const similar = userPhotos();
 const photoListFragment = document.createDocumentFragment();
@@ -7,11 +9,17 @@ const picturesContainer = document.querySelector('.pictures');
 
 similar.forEach(({url, description, likes, comments}) => {
   const templateClone = template.content.cloneNode(true);
+  const picture = templateClone.querySelector('.picture');
   const img = templateClone.querySelector('.picture__img');
   img.src = url;
   img.alt = description;
   templateClone.querySelector('.picture__likes').textContent = likes;
   templateClone.querySelector('.picture__comments').textContent = comments.length;
   photoListFragment.append(templateClone);
+  picture.addEventListener('click', () =>{
+    showBigPicture({url, description, likes, comments});
+  });
 });
 picturesContainer.append(photoListFragment);
+
+
