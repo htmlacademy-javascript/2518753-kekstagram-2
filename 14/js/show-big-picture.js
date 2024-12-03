f2ed5e0c35c70e0c093a8fe0ade6fd1e131d3d27
Keks <keks@htmlacademy.ru>
2024-12-03 06:42:26 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/show-big-picture.js
@@ -0,0 +1,71 @@
+import {CLASS_NAME_HIDDEN} from'./util.js';
+const bigWindow = document.querySelector('.big-picture');
+const bigWindowImg = bigWindow .querySelector('.big-picture__img img');
+const bigWindowLikes = bigWindow .querySelector('.likes-count');
+const btnWindowCancel = bigWindow.querySelector('.big-picture__cancel');
+const bigWindowTotalComment = bigWindow .querySelector('.social__comment-total-count');
+const bigWindowComments = bigWindow.querySelector('.social__comments');
+const socialCaption = bigWindow.querySelector('.social__caption');
+const commentsLoader = bigWindow.querySelector('.comments-loader');
+const btnCommentsLoader = bigWindow.querySelector('.comments-loader');
+const socialCommentShownCount = bigWindow.querySelector('.social__comment-shown-count');
+function closeBigPicture(){
+  bigWindow.classList.add(CLASS_NAME_HIDDEN);
+  document.body.classList.remove('modal-open');
+}
+
+function closeBigPictureEsc(evt){
+  if(evt.key === 'Escape'){
+    closeBigPicture();
+  }
+}
+
+
+function currentListComments(comments) {
+  let currentIndex = 0;
+  const commentsPerPage = 5;
+  const totalComments = comments.length;
+
+  function renderComments() {
+    const currentList = comments.slice(currentIndex, currentIndex + commentsPerPage);
+
+    currentList.forEach(({ id, avatar, name, message }) => {
+      bigWindowComments.insertAdjacentHTML('beforeend', `
+              <li class="social__comment" id="comment-${id}">
+                  <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
+                  <p class="social__text">${message}</p>
+              </li>
+          `);
+    });
+
+    currentIndex += currentList.length;
+
+    socialCommentShownCount.textContent = currentIndex;
+    if (currentIndex >= totalComments) {
+      btnCommentsLoader.style.display = 'none';
+    } else{
+      btnCommentsLoader.style.display = 'block';
+    }
+  }
+
+  btnCommentsLoader.addEventListener('click', renderComments);
+  renderComments();
+}
+
+
+export const showBigPicture = ({url, description, likes, comments})=>{
+  bigWindow.classList.remove(CLASS_NAME_HIDDEN);
+  commentsLoader.classList.remove(CLASS_NAME_HIDDEN);
+
+  bigWindowComments.innerHTML = '';
+  bigWindowImg.src = url;
+  bigWindowLikes.textContent = likes;
+  bigWindowTotalComment.textContent = comments.length;
+  socialCaption.textContent = description;
+  document.body.classList.add('modal-open');
+  currentListComments (comments);
+
+  btnWindowCancel.addEventListener('click', closeBigPicture);
+  document.addEventListener('keydown', closeBigPictureEsc);
+
+};
---
+++ 14/js/util.js
@@ -0,0 +1,4 @@
+export function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+export const CLASS_NAME_HIDDEN = 'hidden';
