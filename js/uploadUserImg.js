c83aa96688c33d5c0b116ef6463fa073cceb0516
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-10 18:29:45 +0300

    реализует модуль загрузки фото пользователя

--- js/uploadUserImg.js
+++ js/uploadUserImg.js
@@ -1,10 +1,13 @@
 import {imgUploadPreview} from'./scale-img.js';
-
+const FILE_TYPES = ['gif','jpg','jpeg','png'];
 const fileChooser = document.querySelector('.img-upload__start input[type="file"]');
 
 fileChooser.addEventListener('change',()=>{
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-
+  const matches = FILE_TYPES.some((it)=>fileName.endsWith(it));
+  if(matches){
+    imgUploadPreview.src = URL.createObjectURL(file);
+  }
 });
