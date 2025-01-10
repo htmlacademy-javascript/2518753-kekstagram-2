b419af68776b559a6abb59758fa15ce62a74c46f
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-10 18:15:14 +0300

    работает в модуле загрузки

--- js/uploadImg.js
+++ js/uploadImg.js
@@ -1,1 +1,10 @@
 import {imgUploadPreview} from'./scale-img.js';
+
+const fileChooser = document.querySelector('.img-upload__start input[type="file"]');
+
+fileChooser.addEventListener('change',()=>{
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+
+});
