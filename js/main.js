1ca5ff0e63b5ae5c6979d8f809db248768b95850
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-19 07:27:24 +0300

    добавляет фильтрацию и debounce

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 import { showErrorMessage } from './messages.js';
 import { getData } from './api.js';
 import { creatPhotos } from './create-photos.js';
-import { setupFilters } from './img-filter.js';
-import './load-form.js';
-import './validation-form.js';
-
-getData().then((pictures) => {
+import { configFilter } from './img-filter.js';
+try {
+  const pictures = await getData();
   creatPhotos(pictures);
-  setupFilters(pictures);
-}).catch(showErrorMessage);
-
+  configFilter(pictures);
+} catch {
+  showErrorMessage();
+}
