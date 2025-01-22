b216c1b718e4db98e5a7516f7b4a0387fee35b96
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-22 14:52:29 +0300

    переделывает main

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ import{showErrorMessage} from './messages.js';
 import { getData } from './api.js';
 import { creatPhotos } from './create-photos.js';
 import{configFilter} from './img-filter.js';
-try{
-  const pictures = await getData();
+
+getData().then((pictures) => {
   creatPhotos(pictures);
   configFilter(pictures);
-} catch{
-  showErrorMessage();
-}
+}).catch(showErrorMessage);
+
