4e9d82490c92daa88f1b6a307a1f93a1faa4e6ed
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-23 13:17:13 +0300

    выполняет тестирование

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -1,5 +1,8 @@
 import { CLASS_NAME_HIDDEN, hasKeyEscape } from './util.js';
 
+let currentIndex = 0;
+let pictureComments = [];
+const COMMENTS_PER_PAGE = 5;
 const bigWindow = document.querySelector('.big-picture');
 const bigWindowImg = bigWindow.querySelector('.big-picture__img img');
 const bigWindowLikes = bigWindow.querySelector('.likes-count');
@@ -9,58 +12,53 @@ const bigWindowComments = bigWindow.querySelector('.social__comments');
 const socialCaption = bigWindow.querySelector('.social__caption');
 const btnCommentsLoader = bigWindow.querySelector('.comments-loader');
 const socialCommentShownCount = bigWindow.querySelector('.social__comment-shown-count');
+const resetCommentsCount = () => {
+  currentIndex = 0;
+};
 const closeBigPicture = () => {
+  resetCommentsCount();
   bigWindow.classList.add(CLASS_NAME_HIDDEN);
   document.body.classList.remove('modal-open');
 };
 
 const closeBigPictureEsc = (evt) => hasKeyEscape(evt) && closeBigPicture();
-
+const createComment = ({ id, avatar, name, message }) => {
+  bigWindowComments.insertAdjacentHTML('beforeend', `
+    <li class="social__comment" id="comment-${id}">
+      <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
+      <p class="social__text">${message}</p>
+    </li>
+  `);
+};
 
 const currentListComments = (comments) => {
-  let currentIndex = 0;
-  const commentsPerPage = 5;
-  const totalComments = comments.length;
-
-  const renderComments = () => {
-    const currentList = comments.slice(currentIndex, currentIndex + commentsPerPage);
-
-    currentList.forEach(({ id, avatar, name, message }) => {
-      bigWindowComments.insertAdjacentHTML('beforeend', `
-              <li class="social__comment" id="comment-${id}">
-                  <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
-                  <p class="social__text">${message}</p>
-              </li>
-          `);
-    });
-
-    currentIndex += currentList.length;
-
-    socialCommentShownCount.textContent = currentIndex;
-    if (currentIndex >= totalComments) {
-      btnCommentsLoader.style.display = 'none';
-    } else {
-      btnCommentsLoader.style.display = 'block';
-    }
-  };
-
-  btnCommentsLoader.addEventListener('click', renderComments);
-  renderComments();
+  currentIndex += COMMENTS_PER_PAGE;
+  if (currentIndex >= comments.length) {
+    btnCommentsLoader.style.display = 'none';
+    currentIndex = comments.length;
+  } else {
+    btnCommentsLoader.style.display = 'block';
+  }
+  bigWindowTotalComment.textContent = comments.length;
+  socialCommentShownCount.textContent = currentIndex;
+  bigWindowComments.innerHTML = '';
+  comments.slice(0, currentIndex).forEach(createComment);
 };
 
 
 export const showBigPicture = ({ url, description, likes, comments }) => {
   bigWindow.classList.remove(CLASS_NAME_HIDDEN);
   btnCommentsLoader.classList.remove(CLASS_NAME_HIDDEN);
-  bigWindowComments.innerHTML = '';
   bigWindowImg.src = url;
   bigWindowLikes.textContent = likes;
   bigWindowTotalComment.textContent = comments.length;
   socialCaption.textContent = description;
   document.body.classList.add('modal-open');
+  pictureComments = comments;
   currentListComments(comments);
 
   btnWindowCancel.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', closeBigPictureEsc);
 
 };
+btnCommentsLoader.addEventListener('click', () => currentListComments(pictureComments));
