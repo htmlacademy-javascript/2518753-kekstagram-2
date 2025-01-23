e053c6d92cd74b00a2ee5cd89c2335016f9832b5
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2025-01-23 12:37:41 +0300

    отправляет e2e тесты

--- js/img-filter.js
+++ js/img-filter.js
@@ -12,24 +12,8 @@ const filterElement = document.querySelector('.img-filters');
 let currentFilter = FILTER.Default;
 let pictures = [];
 const renderDebounce = debounce(creatPhotos);
-function onFilterChange(evt) {
-  const targetButton = evt.target;
-  const activeButton = document.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
-  if (!targetButton.matches('button')) {
-    return;
-  }
-  if (activeButton === targetButton) {
-    return;
-  }
 
-  activeButton.classList.toggle(ACTIVE_BUTTON_CLASS);
-  targetButton.classList.toggle(ACTIVE_BUTTON_CLASS);
-  currentFilter = targetButton.getAttribute('id');
-  applyFilter();
-}
-
-
-function applyFilter() {
+const applyFilter = () => {
   let filteredPictures = [];
   switch (currentFilter) {
     case FILTER.Default:
@@ -44,10 +28,47 @@ function applyFilter() {
       break;
   }
   renderDebounce(filteredPictures);
-}
-export function configFilter(picturesData) {
-  filterElement.classList.remove('img-filters--inactive');
-  filterElement.addEventListener('click', onFilterChange);
-  pictures = picturesData;
 };
+const onFilterChange = (evt) => {
+  function onFilterChange(evt) {
+    const targetButton = evt.target;
+    const activeButton = document.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
+    if (!targetButton.matches('button')) {
+      return;
+    }
+    if (activeButton === targetButton) {
+      return;
+    }
+
+    activeButton.classList.toggle(ACTIVE_BUTTON_CLASS);
+    targetButton.classList.toggle(ACTIVE_BUTTON_CLASS);
+    currentFilter = targetButton.getAttribute('id');
+    applyFilter();
+  };
+
+  export const configFilter = (picturesData) => {
+  }
+
+
+  function applyFilter() {
+    let filteredPictures = [];
+    switch (currentFilter) {
+      case FILTER.Default:
+        filteredPictures = pictures;
+        break;
+
+      case FILTER.Random:
+        filteredPictures = pictures.toSorted(() => 0.5 - Math.random()).slice(0, MAX_COUNT);
+        break;
+
+      case FILTER.Discussed: filteredPictures = pictures.toSorted((a, b) => b.comments.length - a.comments.length);
+        break;
+    }
+    renderDebounce(filteredPictures);
+  }
+  export function configFilter(picturesData) {
+    filterElement.classList.remove('img-filters--inactive');
+    filterElement.addEventListener('click', onFilterChange);
+    pictures = picturesData;
+  };
 
--- js/util.js
+++ js/util.js
@@ -1,11 +1,12 @@
 export const CLASS_NAME_HIDDEN = 'hidden';
 
 export const hasKeyEscape = (event) => event.key === 'Escape';
-export function debounce(callback, timeoutDelay = 500) {
-  let timeoutId;
+export const debounce = (callback, timeoutDelay = 500) => {
+  export function debounce(callback, timeoutDelay = 500) {
+    let timeoutId;
 
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+    return (...rest) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+    };
   };
-};
